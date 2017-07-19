53ad3866b8dfbd012748e4ad7d7ed7025d491bd0
Maurice Yap <mauriceyap@hotmail.co.uk>
2017-07-19 01:32:06 +0100

    REVERT remove application id validation

--- src/alexa-main.py
+++ src/alexa-main.py
@@ -4,6 +4,10 @@ APPLICATION_ID = "amzn1.ask.skill.dd677950-cade-4805-b1f1-ce2e3a3569f0"
 
 
 def lambda_handler(event, context):
+    # Make sure only this Alexa skill can use this function
+    if event['session']['application']['applicationId'] != APPLICATION_ID:
+        raise ValueError("Invalid Application ID")
+
     if event['session']['new']:
         events.on_session_started({'requestId': event['request']['requestId']},
                                   event['session'])
