b45c147eccb7171c1712d9818a3c37333036b6c0
Maurice Yap <mauriceyap@hotmail.co.uk>
2017-08-10 21:40:00 +0100

    add comments for date util function

--- src/utils/date_utils.py
+++ src/utils/date_utils.py
@@ -3,7 +3,6 @@ import re
 
 
 def sunday_from(amazon_date):
-    # TODO
     # Returns a datetime object for the given Amazon.DATE
     if re.match(r"20\d\d-\d\d-\d\d", amazon_date):
         date = datetime.strptime(amazon_date, "%Y-%m-%d").date()
@@ -13,8 +12,10 @@ def sunday_from(amazon_date):
             raise RuntimeError("That day isn't a Sunday. What's the date"
                                " of the service you're after? ")
     elif re.match(r"20\d\d-W\d\d-WE", amazon_date):
+        # Convert weekend to Sunday
         return datetime.strptime(amazon_date[:-3] + "-0", "%Y-W%W-%w").date()
     elif re.match(r"20\d\d-W\d\d", amazon_date):
+        # Convert week to Sunday
         return datetime.strptime(amazon_date + "-0", "%Y-W%W-%w").date()
     else:
         print(amazon_date)
